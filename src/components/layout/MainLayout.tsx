d9353d1e34fa7481adaace86976b3761aeca4a97
tan <13320385140@163.com>
2026-02-10 10:52:31 +0800

    书架页面的主内容区域滚动时不会显示滚动条。

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -31,7 +31,7 @@ export function MainLayout({ children }: MainLayoutProps) {
       <Sidebar currentPage={currentPage} onPageChange={setCurrentPage} />
 
       <div className={`re-main-wrap ${sidebarCollapsed ? 'expanded' : ''}`}>
-        <main className="flex-1 min-h-0 overflow-auto">
+        <main className="flex-1 min-h-0 overflow-auto silent-scroll">
           {children(currentPage)}
         </main>
 
